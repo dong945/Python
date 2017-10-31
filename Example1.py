1e1a6ff4f879fef3e6118807049d0cde9b971a3a
dong945 <dong.tw@gmail.com>
2017-11-01 07:45:15 +0800

    Add files via upload

---
+++ Example1.py
@@ -0,0 +1,50 @@
+from decimal import Decimal
+def sqrt(n): 
+  low = 0 
+  high = n 
+  while high-low > 0.00001: 
+    mid = (low+high) / 2 
+    if mid*mid <= n: 
+      low = mid 
+    else: 
+      high = mid 
+  return low 
+
+def sqrtx(n, x): 
+  low = 0 
+  high = n 
+  while high-low > 0.00001: 
+    mid = (low+high) / 2 
+    if pow(mid, x) <= n: 
+      low = mid 
+    else: 
+      high = mid 
+  return low 
+
+def sqrty(n, x, y): 
+    low = 0 
+    high = n 
+    while high-low > 0.00001: 
+        mid = (low+high) / 2 
+        if pow(mid, y) <= n: 
+          low = mid 
+        else: 
+          high = mid 
+    return pow(low, x) 
+
+def sqrtz(n, x):
+  low = 0
+  high = n
+  for i in range(1, n):
+    if (i ** x) >= n:
+      high = i
+      low = high - 1
+      break
+  while high-low > 0.00001: 
+    mid = (low+high) / 2 
+    if pow(mid, x) <= n: 
+      low = mid 
+    else: 
+      high = mid 
+  return low
+  
